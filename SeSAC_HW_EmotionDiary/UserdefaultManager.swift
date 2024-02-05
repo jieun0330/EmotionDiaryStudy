48d63af748936f70817cff2454f6677ef119926a
jieun <jieun_0330@naver.com>
2024-02-05 14:55:25 +0900

    :white_check_mark: [test] UserDefault 조금(많이) 안것같다, 유레카!

--- SeSAC_HW_EmotionDiary/UserdefaultManager.swift
+++ SeSAC_HW_EmotionDiary/UserdefaultManager.swift
@@ -9,17 +9,75 @@ import Foundation
 
 class UserdefaultManager {
     private init() { }
-    static let shared = UserdefaultManager()
     
-    let userDefault = UserDefaults.standard
+    // static을 안쓸땐 접근이 불가능한처럼 오류가 떴는데
+    // static을 쓰니까 오류가 안난다! 전역 ?
     
-    var clickNum: Int {
+    static func getClickNum(_ tag: Int) -> Int {
+        return UserDefaults.standard.integer(forKey: "\(tag)")
+    }
+    
+    static func setClickNum(_ newValue: Int, tag: Int) {
+        return UserDefaults.standard.set(newValue, forKey: "\(tag)")
+    }
+    
+    /*
+     UserDefaultManager 안에서는 get, set 쓰는 코드가 세트인것마냥 생각을 했는데
+     메서드로도 만들어보고, 매개변수를 넣어보고, return을 넣어서 값을 반환하고, static을 써서 다른 파일에서 쓸 수 있게끔 만들어보니까
+     조금씩 이해가 간다
+     */
+    
+    var clickArr: [Int] {
         get {
-            userDefault.integer(forKey: "clickNum")
+            UserDefaults.standard.array(forKey: "ClickArr") as? [Int] ?? []
+            
+            // 위 코드처럼 쓰는 이유
+            /*
+             let a = UserDefaults.standard.array(forKey: "ClickArr")
+             -> a의 타입을 찍어보면 [Any]가 나온다
+             -> 나는 이걸 [Int] 배열로 만들고 싶으니까
+             -> 타입캐스팅을 한다
+             let b = a as? [Int]
+             -> b의 타입을 찍어보면 [Int]? 옵셔널이니까
+             -> 옵셔널 바인딩을 해준다
+             if let c = b {
+             }
+             그걸 한줄로 쓰면 위에 처럼 된다!!!!!! 유레카~~~~~ 감사합니다 브랜님
+             */
         }
         set {
-            userDefault.set(newValue, forKey: "clickNum")
+            UserDefaults.standard.setValue(newValue, forKey: "ClickArr")
         }
     }
+    
+    //    static let shared = UserdefaultManager()
+    //
+    //    let userDefault = UserDefaults.standard
+    //
+    //    var clickNum: Int {
+    //        get {
+    //            userDefault.integer(forKey: "clickNum")
+    //        }
+    //        set {
+    //            userDefault.set(newValue, forKey: "clickNum")
+    //        }
+    //    }
 }
 
+// 🚨🚨🚨🚨🚨원래 나의 목적🚨🚨🚨🚨🚨
+// UserDefaultManager를 이용해서 저장한 값을 세팅하려고 했는데
+
+// clickNum 자리에 tagNum이 들어와야되는거잖아
+
+// tagNum에 맞게 저장되는 곳이 한곳이 아니라 9곳인거잖아??
+// 그럼 recap때처럼 단순히 닉네임 하나를 저장하고 세팅해주는게 아니라
+
+// 버튼마다 저장되는 숫자가 달라야 하는건데
+// 그럼 array[tagNum] 하면 되는거같아서 했는데
+// key 값은 string이여야 하잖아?
+// 그럼 "array[0]" = "0"
+// "array[1]" = "0"
+// 뭔데
+// "0" 이라는 똑같은 공간에 저장되고있는거잖아
+// 그럼 배열을 써야 되는건가 해서
+// 그래서 배열의 길로 샘
--- SeSAC_HW_EmotionDiary/ViewController.swift
+++ SeSAC_HW_EmotionDiary/ViewController.swift
@@ -14,11 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet var emotionLabel: [UILabel]!
     @IBOutlet var resetButton: UIButton!
     
-//    let array = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    var array = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-
     var emotionList = ["행복해", "사랑해", "좋아해", "당황해", "속상해", "우울해", "심심해", "행복해", "행복해"]
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -44,53 +41,30 @@ class ViewController: UIViewController {
     func setButton(_ sender: UIButton) {
         let tagNum = sender.tag
         emotionButton[tagNum].setImage(UIImage(named: "slime\(tagNum+1)"), for: .normal)
-        
-        // 🚨🚨🚨🚨🚨원래 나의 목적🚨🚨🚨🚨🚨
-        // UserDefaultManager를 이용해서 저장한 값을 세팅하려고 했는데
-        // tagNum에 맞게 저장되는 곳이 한곳이 아니라 9곳인거잖아??
-        // 그럼 recap때처럼 단순히 닉네임 하나를 저장하고 세팅해주는게 아니라
-        
-        // 버튼마다 저장되는 숫자가 달라야 하는건데
-        // 그럼 array[tagNum] 하면 되는거같아서 했는데
-        // key 값은 string이여야 하잖아?
-        // 그럼 "array[0]" = "0"
-        // "array[1]" = "0"
-        // 뭔데
-        // "0" 이라는 똑같은 공간에 저장되고있는거잖아
-        // 그럼 배열을 써야 되는건가 해서
-        // 그래서 배열의 길로 샘
-        
-        emotionLabel[tagNum].text = "\(emotionList[tagNum]) \(UserDefaults.standard.integer(forKey: "\(tagNum)"))"
-//        emotionLabel[tagNum].text = "\(emotionList[tagNum]) \(UserDefaults.standard.integer(forKey: "\(array[tagNum])"))"
-
-
+        emotionLabel[tagNum].text = "\(emotionList[tagNum]) \(UserdefaultManager.getClickNum(tagNum))"
         
         // 1. array[tagNum] -> array[0], array[1], ... array[8]
         // 다 0이 나와서 0ㅇ으로 세팅되겠지
-
+        
     }
     
     @IBAction func buttonClicked(_ sender: UIButton, index: Int) {
         let tagNum = sender.tag
-        print(tagNum)
-//        let count = UserDefaults.standard.integer(forKey: "\(tagNum)")
-//        let countUp = count + 1
-//        UserDefaults.standard.set(countUp, forKey: "\(tagNum)")
-//        emotionLabel[tagNum].text = "\(emotionList[tagNum]) \(UserDefaults.standard.integer(forKey: "\(tagNum)"))"
+        //        let count = UserDefaults.standard.integer(forKey: "\(tagNum)")
+        //        UserDefaults.standard.set(countUp, forKey: "\(tagNum)")
+        //        emotionLabel[tagNum].text = "\(emotionList[tagNum]) \(UserDefaults.standard.integer(forKey: "\(tagNum)"))"
         
         // 3. count의 숫자는 array[tagNum] 에 있는 숫자로 만들거야
-        let count = UserDefaults.standard.integer(forKey: "\(tagNum)")
-//        let count = UserDefaults.standard.array(forKey: array)
-//        let count = UserDefaults.standard.object(forKey: array )
+        //        let count = UserDefaults.standard.integer(forKey: "\(tagNum)")
+        let count = UserdefaultManager.getClickNum(tagNum)
         // 2. 클릭되서 +1이 되게 만들거야
         let countUp = count + 1
         // 4. 그럼 이 countUp된 숫자가 다시 array[tagNum]에 저장되어야겠지?
-        UserDefaults.standard.set(countUp, forKey: "\(tagNum)")
-        print(array[tagNum])
+        //        UserDefaults.standard.set(countUp, forKey: "\(tagNum)")
+        UserdefaultManager.setClickNum(countUp, tag: tagNum)
         // 5. 그럼 이 저장된 숫자를 보여줄거야
-        emotionLabel[tagNum].text = "\(emotionList[tagNum]) \(UserDefaults.standard.integer(forKey: "\(tagNum)"))"
-
-        
+        //        emotionLabel[tagNum].text = "\(emotionList[tagNum]) \(UserDefaults.standard.integer(forKey: "\(tagNum)"))"
+        emotionLabel[tagNum].text = "\(emotionList[tagNum]) \(UserdefaultManager.getClickNum(tagNum))"        
     }
     
     @IBAction func resetButtonClicked(_ sender: UIButton) {
